ee74db83fb1f8373a164659143d3c816f066d450
aswin-code <aswinpops1234@gmail.com>
2023-01-05 17:28:22 +0530

    follower list from profile

--- controller/connection.js
+++ controller/connection.js
@@ -52,7 +52,7 @@ exports.follow = async (req, res) => {
 }
 exports.getConnection = async (req, res) => {
     try {
-        const connection = await connectionModel.findOne({ userid: req.params.id || req.user }).populate('followers', '-password').populate('following', '-password')
+        const connection = await connectionModel.findOne({ userid: req.params.id || req.user }).populate('followers', '-password -refreshToken').populate('following', '-password -refreshToken')
         res.status(200).json(connection)
     } catch (error) {
         console.log(error)
